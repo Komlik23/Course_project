9197a4b08b9d6ef53d6366421ec7f674f4124929
Ivan <2014ivanee@gmail.com>
2017-09-19 23:17:20 +0300

    added uploading of languages

--- Course_with_angular/wwwroot/js/app.js
+++ Course_with_angular/wwwroot/js/app.js
@@ -1,40 +1,13 @@
 ﻿
 
 
-var app2 = angular.module("myApp1", []);
+var app2 = angular.module("myApp1", ['pascalprecht.translate']);
 
 app2.config(["$translateProvider", function ($translateProvider) {
-
-    var en_translations = {
-        "language": "Selected Language English",
-        "greeting": "Welcome Dinesh",
-        "Phone":"Phone",
-        "Support": "Support",
-        "Marketing": "Marketing",
-        "Project": "Project",
-        "Confirmation": "Thank you for confirming your email. Please ",
-        "ConfirmationLink": " Click here to Log in",
-        "Home": "Home",
-        "About": "About"
-
-    };
-
-    var ru_translations = {
-        "language": "Selected Language Spanish",
-        "greeting": "Bienvenida Dinesh",
-        "Phone":"Phone",
-        "Support": "Support",
-        "Marketing": "Marketing",
-        "Project": "Project",
-        "Confirmation": "Thank you for confirming your email. Please ",
-        "ConfirmationLink": " Нажмите здесь чтобы войти",
-        "Home": "Главная",
-        "About": "О нас"
-    };
-
-    $translateProvider.translations('en', en_translations);
-
-    $translateProvider.translations('ru', ru_translations);
+    $translateProvider.useStaticFilesLoader({
+        prefix: '../../resources/translation_',
+        suffix: '.json'
+    })
 
     $translateProvider.preferredLanguage('en');
 
